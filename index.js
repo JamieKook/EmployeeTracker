8e037af17253a644cc5ba203354b0acdca146170
Jamie <kookjc6@gmail.com>
2020-01-21 21:15:13 -0600

    added select query to gain data from database

--- index.js
+++ index.js
@@ -12,5 +12,13 @@ const connection = mysql.createConnection({
 connection.connect(function(err) {
     if (err) throw err; 
     console.log("connected as id "+ connection.threadId); 
-    connection.end(); 
+    afterConnection(); 
 }); 
+
+function afterConnection() {
+    connection.query("SELECT E.id, E.first_name, E.last_name, R.title, D.name, R.salary FROM employee as E JOIN role as R on E.role_id = R.id JOIN department as D on D.id = R.department_id", function(err, res) {
+      if (err) throw err;
+      console.log(res);
+      connection.end();
+    });
+}
